78e65a06ea6d3aba7f0146568c714cb6b78e2e65
LION TUNNEL <mrdoe0435@gmail.com>
2025-03-19 07:26:24 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -14,12 +14,12 @@ ISP=$(cat /etc/xray/isp)
 CITY=$(cat /etc/xray/city)
 author=$(cat /etc/profil)
 DATE2=$(date -R | cut -d " " -f -5)
-Exp2=$(curl -sS https://raw.githubusercontent.com/FanoraSsh/permission/main/ip | grep $MYIP | awk '{print $3}')
+Exp2=$(curl -sS https://raw.githubusercontent.com/liontunnel/permission/main/ip | grep $MYIP | awk '{print $3}')
 export RED='\033[0;31m'
 export GREEN='\033[0;32m'
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/FanoraSsh/permission/main/ip"
+data_ip="https://raw.githubusercontent.com/liontunnel/permission/main/ip"
 checking_sc() {
 useexp=$(curl -sS $data_ip | grep $MYIP | awk '{print $3}')
 if [[ $date_list < $useexp ]]; then
@@ -412,7 +412,7 @@ menu
 function updatews(){
 cd
 rm -rf *
-wget https://raw.githubusercontent.com/FanoraSsh/autoscriptssh/main/m-update.sh
+wget https://raw.githubusercontent.com/liontunnel/script/main/m-update.sh
 clear
 chmod +x m-update.sh && ./m-update.sh
 }
