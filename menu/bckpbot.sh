9f69d667a67e366094af7bb2345b06a5857712a7
LION TUNNEL <mrdoe0435@gmail.com>
2025-03-19 07:53:16 +0700

    Update bckpbot.sh

--- menu/bckpbot.sh
+++ menu/bckpbot.sh
@@ -13,7 +13,7 @@ WH='\033[1;37m'
 ipsaya=$(curl -sS ipinfo.io/ip)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/FanoraSsh/permission/main/ip"
+data_ip="https://raw.githubusercontent.com/liontunnel/permission/main/ip"
 checking_sc() {
     useexp=$(curl -sS $data_ip | grep $ipsaya | awk '{print $3}')
     if [[ $date_list < $useexp ]]; then
@@ -27,7 +27,7 @@ checking_sc() {
         echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
         echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
         echo -e "             \033[0;33mContact Admin :${NC}"
-        echo -e "     \033[0;36mTelegram${NC}: https://t.me/Fanorassh"
+        echo -e "     \033[0;36mTelegram${NC}: https://t.me/FanoraVpnTunnel"
         echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
         exit
     fi
